de5a78ab99b4f2cec5a28f1d12dea3bd8067ebcb
Chad Redmond <credmond@mercyhurst.edu>
2026-03-25 22:28:55 +0000

    players endpoint

--- main.py
+++ main.py
@@ -15,7 +15,19 @@ async def get_years():
 @app.get("/teams")
 async def get_teams(year: int):
     with Session(engine) as session:
-        result = session.exec(select(Teams.name, Teams.lgID, Teams.divID).where(Teams.yearID == year).order_by(Teams.name))
-        return [{"name": name, "lgID": lgID, "divID": divID} for name, lgID, divID in result.all()]
+        result = session.exec(select(Teams.teamID, Teams.name, Teams.lgID, Teams.divID).where(Teams.yearID == year).order_by(Teams.name))
+        return [{"teamID": teamID, "name": name, "lgID": lgID, "divID": divID} for teamID, name, lgID, divID in result.all()]
+
+@app.get("/players")
+async def get_players(teamID: str, yearID: int):
+    with Session(engine) as session:
+        result = session.exec(
+            select(Batting.playerID, People.nameFirst, People.nameLast)
+            .where(Batting.teamID == teamID, Batting.yearID == yearID)
+            .join(People, Batting.playerID == People.playerID)
+            .distinct()
+            .order_by(People.nameLast, People.nameFirst)
+        )
+        return [{"playerID": playerID, "nameFirst": nameFirst, "nameLast": nameLast} for playerID, nameFirst, nameLast in result.all()]
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
